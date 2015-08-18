862e374897f7c5f7b67d364c0848eff2045e70ea
BarbaraGouvea <barbarasjc12010@hotmail.com>
2015-08-18 20:00:56 -0300

    aula 18_08 mais classes

---
+++ HelloWorldJava/src/PontoFlutuante.java
@@ -0,0 +1,11 @@
+
+public class PontoFlutuante {
+	public static void main (String []args){
+	float f =3.5f;
+	
+	}
+}
+/* Incode: é a maneira de representação em questão de bytes.
+ * unicode é uma tabela ideal de representação numerica em caracteres.
+ * TF8
+ */
