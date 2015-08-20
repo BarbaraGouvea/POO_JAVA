da999ffce4fecd5f910d42db68a419f58b49191b
BarbaraGouvea <barbarasjc12010@hotmail.com>
2015-08-20 20:08:22 -0300

    Aulas dia 19_8 e reposição. Início tarefa 2.

---
+++ Aula04_19_08/src/Mapas.java
@@ -0,0 +1,27 @@
+import java.util.Map;
+import java.util.HashMap;
+
+
+public class Mapas {
+
+	public static void main(String[] args) {
+		Map<String, String>paises= new HashMap<String, String>();
+		paises.put("Brasil", "Portugues");
+		paises.put("Argentina", "Espanhol");
+		System.out.println(paises.get("Argentina"));
+		System.out.println(paises.containsKey("Argentina"));
+		paises.remove("Argentina");
+		paises.remove("Argentina");//pra ver se dá pau se tentar remover um que ja não tem mais
+		
+		for (String chave: paises.keySet())
+		{
+			System.out.println(chave+":"+paises.get(chave));
+		}
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Aula04_19_08/src/vetores.java
@@ -0,0 +1,30 @@
+
+public class vetores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] vet_inteiros=new int [6];
+		
+		for (int i: vet_inteiros){
+			System.out.println(vet_inteiros[i]);
+			
+		}
+		
+		//String [] nomes= new String [10];
+		//String [] nomes= new String [2];
+		//nomes[0]="BAbi";
+		//nomes[1]="Gouvea";
+		String []nomes={"BAbi","Gouvea"};
+		
+		for (String n:nomes){
+			System.out.println(nomes.length);
+		}
+		
+		// int inteiros []=new int [6];
+		// int [] inteiros []=new int [6] -> É matriz
+		// Quado for objeto 
+
+	}
+
+}
