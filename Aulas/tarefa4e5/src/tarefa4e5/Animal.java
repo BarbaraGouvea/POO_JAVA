b9e3419a52997643d4dc1b3ae9a86365ab3366c3
BarbaraGouvea <barbarasjc12010@hotmail.com>
2015-09-03 16:46:34 -0300

    tarefa 4  animal e Noh - OFI

---
+++ Aulas/tarefa4e5/src/tarefa4e5/Animal.java
@@ -0,0 +1,22 @@
+package tarefa4e5;
+
+	public class Animal {
+			public String raca;
+			public int idade;
+			public float peso;
+		
+		public Animal (String raca, int idade, float peso){
+			this.raca=raca;
+			this.idade=idade;
+			this.peso=peso;
+			
+			
+		}
+		
+		public String fazerBarulho(){
+			return "O barullho feito pelo animal é: ";
+			
+			
+		}
+		
+}
